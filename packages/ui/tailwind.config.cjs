761e44be603d9d09cddb098ca2b2f4db10684401
김영민 <youngmin@gim-yeongmin-ui-MacBookAir.local>
2025-09-06 23:20:06 +0900

    style: format

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{ts,tsx}",
-    "../../apps/web/app/**/*.{ts,tsx}",
-    "../../apps/web/components/**/*.{ts,tsx}",
+    './src/**/*.{ts,tsx}',
+    '../../apps/web/app/**/*.{ts,tsx}',
+    '../../apps/web/components/**/*.{ts,tsx}',
   ],
   theme: { extend: {} },
   plugins: [],
