0120efd14faf4db354512f2ca62ca37fd3d16db8
youngmin <0mini9939@gmail.com>
2025-09-07 18:05:58 +0900

    fix: use cn utils

--- packages/ui/src/components/button/button.tsx
+++ packages/ui/src/components/button/button.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/utils';
 import * as React from 'react';
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
@@ -6,10 +7,11 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 
 export const Button: React.FC<Props> = ({ label, children, ...props }) => (
   <button
-    className={`
-      rounded-lg px-3 py-2 cursor-pointer text-white 
-      bg-blue-500 hover:bg-blue-600 active:bg-blue-700 transition-colors 
-      disabled:opacity-50 disabled:cursor-not-allowed`}
+    className={cn(
+      'rounded-lg px-3 py-2 cursor-pointer text-white',
+      'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 transition-colors',
+      'disabled:opacity-50 disabled:cursor-not-allowed',
+    )}
     {...props}
   >
     {children || label}
