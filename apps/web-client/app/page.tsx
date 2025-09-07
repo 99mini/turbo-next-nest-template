c5c07ad9acd23f0c28b2e6963b37d1328c6e8ab7
youngmin <0mini9939@gmail.com>
2025-09-08 07:27:49 +0900

    chore: nextjs safe build

--- apps/web-client/app/page.tsx
+++ apps/web-client/app/page.tsx
@@ -1,13 +1,36 @@
 import { Button } from '@packages/ui';
 
-export default async function Home() {
-  const nextjsRes = await fetch('http://localhost:3000/api');
-  const nextJsData = await nextjsRes.json();
-  const nextJsMessage = nextJsData?.message;
+const getNextjsApi = async (): Promise<{ message: string }> => {
+  try {
+    const res = await fetch('/api');
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching Next.js API:', error);
+    return { message: 'Error fetching Next.js API' };
+  }
+};
 
-  const serverRes = await fetch('http://localhost:8080/');
-  const serverData = await serverRes.json();
-  const serverMessage = serverData?.message;
+const getThirdPartyApi = async (): Promise<{ message: string }> => {
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+  if (!baseUrl) {
+    console.error('NEXT_PUBLIC_SERVER_URL is not defined');
+    return { message: 'Server URL not defined' };
+  }
+  try {
+    const res = await fetch(`${baseUrl}/`);
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching third-party API:', error);
+    return { message: 'Error fetching third-party API' };
+  }
+};
+
+export default async function Home() {
+  const nextJsData = await getNextjsApi();
+  const nextJsMessage = nextJsData.message;
+
+  const serverData = await getThirdPartyApi();
+  const serverMessage = serverData.message;
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
